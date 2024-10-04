23e4774982b1154dcdee26e32b17b25a9acbe924
GiorgioDordo <djole6789@gmail.com>
2024-10-04 17:24:27 +0200

    added some layout fixes

--- esercizio-4-classi/index.php
+++ esercizio-4-classi/index.php
@@ -249,53 +249,32 @@ $classi = [
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
     <main>
-        <div>
+        <div class="container-fluid d-flex justify-content-center m-auto row ">
             <?php foreach ($classi as $singleClass =>  $inSingleClass) { ?>
-            <ul>
-                <h1><?= $singleClass ?></h1>
-                <?php foreach ($inSingleClass as $singleClass) { ?>
-                <li>
-                    <p><strong>ID:</strong><?= $singleClass["id"] ?></p>
-                    <p><strong>NOME:</strong><?= $singleClass["nome"] ?></p>
-                    <p><strong>COGNOME:</strong><?= $singleClass["cognome"] ?></p>
-                    <p><strong>ANNI:</strong><?= $singleClass["anni"] ?></p>
-                    <p><strong>VOTO MEDIO:</strong><?= $singleClass["voto_medio"] ?></p>
-                    <p><strong>LINGUAGGIO PREFERITO:</strong><?= $singleClass["linguaggio_preferito"] ?></p>
-                    <div>
-                        <img class="img'fluid" src="https://source.unsplash.com/random/200x200?person"
-                            alt="randomPerson">
-                    </div>
-                </li>
-                <?php } ?>
-            </ul>
-            <?php } ?>
-        </div>
-
-        <div class="container m-auto row">
-            <?php foreach ($classi as $singleClass =>  $inSingleClass) { ?>
-            <div class="text-center">
-                <h1><?= $singleClass ?></h1>
-            </div>
-            <?php foreach ($inSingleClass as $singleClass) { ?>
-            <div class="col d-flex justify-content-center mb-5">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="https://robohash.org/ <?= $singleClass["nome"] ?>"
-                        alt="Card image cap">
-                    <div class="card-body">
-                        <p><strong>ID:</strong><?= $singleClass["id"] ?></p>
-                        <p><strong>NOME:</strong><?= $singleClass["nome"] ?></p>
-                        <p><strong>COGNOME:</strong><?= $singleClass["cognome"] ?></p>
-                        <p><strong>ANNI:</strong><?= $singleClass["anni"] ?></p>
-                        <p><strong>VOTO MEDIO:</strong><?= $singleClass["voto_medio"] ?></p>
-                        <p><strong>LINGUAGGIO PREFERITO:</strong><?= $singleClass["linguaggio_preferito"] ?></p>
-                    </div>
+                <div class="text-center">
+                    <h1><?= $singleClass ?></h1>
                 </div>
-            </div>
-            <?php } ?>
+                <?php foreach ($inSingleClass as $singleClass) { ?>
+                    <div class="col-2 mb-5">
+                        <div class="card" style="width: 17rem;">
+                            <img class="card-img-top" src="https://robohash.org/ <?= $singleClass["nome"] ?>"
+                                alt="Card image cap">
+                            <div class="card-body">
+                                <p><strong>ID:</strong><?= $singleClass["id"] ?></p>
+                                <p><strong>NOME:</strong><?= $singleClass["nome"] ?></p>
+                                <p><strong>COGNOME:</strong><?= $singleClass["cognome"] ?></p>
+                                <p><strong>ANNI:</strong><?= $singleClass["anni"] ?></p>
+                                <p><strong>VOTO MEDIO:</strong><?= $singleClass["voto_medio"] ?></p>
+                                <p><strong>LINGUAGGIO PREFERITO:</strong><?= $singleClass["linguaggio_preferito"] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
             <?php } ?>
         </div>
     </main>
