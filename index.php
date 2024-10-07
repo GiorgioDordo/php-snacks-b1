eba374c374e46e4eb2b17f47c353f6d64a47fb18
GiorgioDordo <djole6789@gmail.com>
2024-10-07 12:30:34 +0200

    almost

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-function isPalindrome($word) {
-    $reversedWord = '';
-    for ($i = strlen($word) - 1; $i >= 0; $i--) {
-        $reversedWord .=
-    }
-}
-?>
---
+++ snack-5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php
+function isPalindrome($word)
+{
+    $reversedWord = '';
+    for ($i = strlen($word) - 1; $i >= 0; $i--) {
+        $reversedWord .= $word[$i];
+    }
+    return $reversedWord;
+}
+print_r(isPalindrome("Marco"));
+?>
